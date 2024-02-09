a8748cf3a3e4de426abeb687a1206064ce6bbfdd
Aino Spring <info@aino-spring.com>
2024-02-09 17:13:15 +0100

    Admin user page ui working and delete button

--- server/api.go
+++ server/api.go
@@ -10,202 +10,217 @@ import (
 )
 
 type PostPreset struct {
-  Title string `json:"title" binding:"required"`
-  Abstract string `json:"abstract" binding:"required"`
-  Contents string `json:"contents" binding:"required"`
-  Public bool `json:"public" binding:"required"`
+	Title    string `json:"title" binding:"required"`
+	Abstract string `json:"abstract" binding:"required"`
+	Contents string `json:"contents" binding:"required"`
+	Public   bool   `json:"public" binding:"required"`
 }
 
 type UserPreset struct {
-  Email string `json:"email" binding:"required"`
-  Name string `json:"name" binding:"required"`
-  Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Name     string `json:"name" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 func (server *Server) SetupApiPages() {
-  server.Router.POST("/api/posts/:id/edit", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    if !isAdmin {
-      c.JSON(http.StatusForbidden, gin.H{})
-      return
-    }
-    id := c.Param("id")
-    var preset PostPreset
-    c.BindJSON(&preset)
+	server.Router.POST("/api/posts/:id/edit", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		id := c.Param("id")
+		var preset PostPreset
+		c.BindJSON(&preset)
 
-    err := errors.Join(
-      server.Database.SetPostTitle(id, preset.Title),
-      server.Database.SetPostAbstract(id, preset.Abstract),
-      server.Database.SetPostContents(id, preset.Contents),
-      server.Database.SetPostPublic(id, preset.Public),
-      )
+		err := errors.Join(
+			server.Database.SetPostTitle(id, preset.Title),
+			server.Database.SetPostAbstract(id, preset.Abstract),
+			server.Database.SetPostContents(id, preset.Contents),
+			server.Database.SetPostPublic(id, preset.Public),
+		)
 
-    if err != nil {
-      log.Println(err)
-      c.JSON(http.StatusInternalServerError, gin.H{})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{})
-  })
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{})
+	})
 
-  server.Router.POST("/api/new-post", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    if !isAdmin {
-      c.JSON(http.StatusForbidden, gin.H{})
-      return
-    }
-    var preset PostPreset
-    c.BindJSON(&preset)
-    id, err := server.Database.NewPost(preset.Title, preset.Abstract, preset.Contents, preset.Public);
-    
-    if err != nil {
-      log.Println(err)
-      c.JSON(http.StatusInternalServerError, gin.H{})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{"id": id})
-  })
+	server.Router.POST("/api/new-post", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		var preset PostPreset
+		c.BindJSON(&preset)
+		id, err := server.Database.NewPost(preset.Title, preset.Abstract, preset.Contents, preset.Public)
 
-  server.Router.POST("/api/posts/:id/delete", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    if !isAdmin {
-      c.JSON(http.StatusForbidden, gin.H{})
-      return
-    }
-    id := c.Param("id")
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"id": id})
+	})
 
-    err := server.Database.DeletePost(id)
+	server.Router.POST("/api/posts/:id/delete", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		id := c.Param("id")
 
-    if err != nil {
-      log.Println(err)
-      c.JSON(http.StatusInternalServerError, gin.H{})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{})
-  })
+		err := server.Database.DeletePost(id)
 
-  server.Router.POST("/api/signup", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    allowPublicSignup := server.Database.GetSetting("allow_public_signup").(bool)
-    if !(isAdmin || allowPublicSignup) {
-      c.JSON(http.StatusForbidden, gin.H{"code": 0})
-      return
-    }
-    var preset UserPreset
-    c.BindJSON(&preset)
-    _, err := mail.ParseAddress(preset.Email)
-    if err != nil {
-      c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 1})
-      return
-    }
-    _, err = server.Database.FetchUserByName(preset.Name)
-    if err == nil {
-      c.JSON(http.StatusConflict, gin.H{"code": 2})
-      return
-    }
-    id, err := NewUser(server.Database, preset.Email, preset.Name, preset.Password)
-    if err != nil {
-      c.JSON(http.StatusConflict, gin.H{"code": 3})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{"id": id})
-  })
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{})
+	})
 
-  server.Router.GET("/api/login", func (c *gin.Context) {
-    isAuthed, isAdmin := server.CheckContext(c)
-    c.JSON(http.StatusOK, gin.H{"authed": isAuthed, "admin": isAdmin})
-  })
+	server.Router.POST("/api/signup", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		allowPublicSignup := server.Database.GetSetting("allow_public_signup").(bool)
+		if !(isAdmin || allowPublicSignup) {
+			c.JSON(http.StatusForbidden, gin.H{"code": 0})
+			return
+		}
+		var preset UserPreset
+		c.BindJSON(&preset)
+		_, err := mail.ParseAddress(preset.Email)
+		if err != nil {
+			c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 1})
+			return
+		}
+		_, err = server.Database.FetchUserByName(preset.Name)
+		if err == nil {
+			c.JSON(http.StatusConflict, gin.H{"code": 2})
+			return
+		}
+		id, err := NewUser(server.Database, preset.Email, preset.Name, preset.Password)
+		if err != nil {
+			c.JSON(http.StatusConflict, gin.H{"code": 3})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"id": id})
+	})
 
-  server.Router.POST("/api/users/:email/name/set/:value", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    if !isAdmin {
-      c.JSON(http.StatusForbidden, gin.H{})
-      return
-    }
-    email := c.Param("email")
-    value := c.Param("value")
+	server.Router.GET("/api/login", func(c *gin.Context) {
+		isAuthed, isAdmin := server.CheckContext(c)
+		c.JSON(http.StatusOK, gin.H{"authed": isAuthed, "admin": isAdmin})
+	})
 
-    err := server.Database.SetUserName(email, value)
+	server.Router.POST("/api/users/:email/name/set/:value", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		email := c.Param("email")
+		value := c.Param("value")
 
-    if err != nil {
-      log.Println(err)
-      c.JSON(http.StatusInternalServerError, gin.H{})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{})
-  })
+		err := server.Database.SetUserName(email, value)
 
-  server.Router.POST("/api/users/:email/email/set/:value", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    if !isAdmin {
-      c.JSON(http.StatusForbidden, gin.H{})
-      return
-    }
-    email := c.Param("email")
-    value := c.Param("value")
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{})
+	})
 
-    err := server.Database.SetUserEmail(email, value)
+	server.Router.POST("/api/users/:email/email/set/:value", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		email := c.Param("email")
+		value := c.Param("value")
 
-    if err != nil {
-      log.Println(err)
-      c.JSON(http.StatusInternalServerError, gin.H{})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{})
-  })
+		err := server.Database.SetUserEmail(email, value)
 
-  server.Router.POST("/api/users/:email/is-admin/set/:value", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    if !isAdmin {
-      c.JSON(http.StatusForbidden, gin.H{})
-      return
-    }
-    email := c.Param("email")
-    value := c.Param("value")
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{})
+	})
 
-    err := server.Database.SetUserIsAdmin(email, value == "true")
+	server.Router.POST("/api/users/:email/is-admin/set/:value", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		email := c.Param("email")
+		value := c.Param("value")
 
-    if err != nil {
-      log.Println(err)
-      c.JSON(http.StatusInternalServerError, gin.H{})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{})
-  })
+		err := server.Database.SetUserIsAdmin(email, value == "true")
 
-  server.Router.POST("/api/users/:email/password/set/:value", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    if !isAdmin {
-      c.JSON(http.StatusForbidden, gin.H{})
-      return
-    }
-    email := c.Param("email")
-    value := c.Param("value")
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{})
+	})
 
-    err := SetUserPassword(server.Database, email, value)
+	server.Router.POST("/api/users/:email/password/set/:value", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		email := c.Param("email")
+		value := c.Param("value")
 
-    if err != nil {
-      log.Println(err)
-      c.JSON(http.StatusInternalServerError, gin.H{})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{})
-  })
+		err := SetUserPassword(server.Database, email, value)
 
-  server.Router.POST("/api/settings/:key/set/:value", func (c *gin.Context) {
-    _, isAdmin := server.CheckContext(c)
-    if !isAdmin {
-      c.JSON(http.StatusForbidden, gin.H{})
-      return
-    }
-    key := c.Param("key")
-    value := c.Param("value")
-    err := server.Database.SetSetting(key, value)
-    if err != nil {
-      c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
-      return
-    }
-    c.JSON(http.StatusOK, gin.H{})
-  })
+		if err != nil {
+			log.Println(err)
+			c.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{})
+	})
+
+	server.Router.POST("/api/users/:email/delete", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		email := c.Param("email")
+
+		err := server.Database.DeleteUser(email)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{})
+	})
+
+	server.Router.POST("/api/settings/:key/set/:value", func(c *gin.Context) {
+		_, isAdmin := server.CheckContext(c)
+		if !isAdmin {
+			c.JSON(http.StatusForbidden, gin.H{})
+			return
+		}
+		key := c.Param("key")
+		value := c.Param("value")
+		err := server.Database.SetSetting(key, value)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": err})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{})
+	})
 }
-
--- static/scripts/users.js
+++ static/scripts/users.js
@@ -1,4 +1,9 @@
-function setUserField(email, field, value) {
+function showError(message, settingsError) {
+  settingsError.style.display = "block";
+  settingsError.innerHTML = message;
+}
+
+function setUserField(email, field, value, settingsError) {
   let host = location.protocol + "//" + location.host;
   let url = host + "/api/users/" + email + "/" + field + "/set/" + value;
   fetch(url, {
@@ -8,26 +13,59 @@ function setUserField(email, field, value) {
       "Content-type": "application/json; charset=UTF-8"
     }
   }).then((response) => {
-      console.log(response.status);
+      if (response.status != 200) {
+        response.json().then((data) => {
+          showError(data.msg, settingsError);
+        })
+      }
+    });
+}
+
+function deleteUser(email, settingsError) {
+  let host = location.protocol + "//" + location.host;
+  let url = host + "/api/users/" + email + "/delete";
+  fetch(url, {
+    method: "post",
+    body: JSON.stringify({}),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  }).then((response) => {
+      if (response.status != 200) {
+        response.json().then((data) => {
+          showError(data.msg, settingsError);
+        })
+      } else {
+        window.location.reload();
+      }
     });
 }
 
 function setupEntries() {
+  let settingsError = document.querySelector("#settings-error");
+
   let users = document.getElementsByClassName("user");
   for (let idx = 0; idx < users.length; idx++) {
     let user = users[idx];
     let isAdmin = user.querySelector(".is-admin").innerText == "true";
-    let email = user.querySelector(".email").innerText;
-    let isAdminEdit = user.querySelector(".is-admin-edit");
+    let email = user.querySelector("td > .email").innerText;
+    let isAdminEdit = user.querySelector("td > .is-admin-edit");
     isAdminEdit.checked = isAdmin;
     isAdminEdit.oncheckclick = function () {
-      setUserField(email, "is-admin", this.checked);
+      settingsSuccess.style.display = "none";
+      settingsError.style.display = "none";
+      setUserField(email, "is-admin", this.checked, settingsError);
+    }
+    let deleteButton = user.querySelector("td .delete");
+    deleteButton.onclick = function () {
+      settingsError.style.display = "none";
+      deleteUser(email, settingsError);
     }
   }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  
+
   setupEntries();
 
 });
