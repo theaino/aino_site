3e02beea1f5254111811bc10a017a17e6ca18b32
Aino Spring <info@aino-spring.com>
2024-05-08 18:54:49 +0200

    Titles updated

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -1,4 +1,4 @@
-const titles = ["Hi, I'm Aino.", "Hello, World!", "Hi, I write code.", "Work in progess."];
+const titles = ["Spring is in the air", "Hi, I'm Aino.", "Hi, I write code.", "Work in progess."];
 
 function prefix(words){
   if (!words[0] || words.length == 1) return words[0] || "";
