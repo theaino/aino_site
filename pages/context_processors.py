846a766de189f027205c544f2a9a1b5f7ea69508
Aino Spring <aino@fedora.fritz.box>
2024-08-12 10:05:03 +0200

    Footer site on github

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -16,6 +16,7 @@ def load_contact(request):
     return {
         "CONTACT": {
             "github": "https://github.com/AinoSpring",
+            "github_site": "https://github.com/AinoSpring/aino_site",
             "instagram": "https://instagram.com/ainospring",
             "email": "info@aino-spring.com",
         }
