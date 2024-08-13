5b104e14ade033b18a29b7e9ca96512588685093
Aino Spring <aino@fedora.fritz.box>
2024-08-13 21:21:11 +0200

    Code block style and copy button

--- pages/management/commands/generate_pygments_css.py
+++ pages/management/commands/generate_pygments_css.py
@@ -1,6 +1,6 @@
 import os
 from django.core.management.base import BaseCommand
-from pygments.formatters import HtmlFormatter
+from django.conf import settings
 
 class Command(BaseCommand):
     help = "Generate Pygments CSS for code highlighting"
@@ -12,8 +12,7 @@ class Command(BaseCommand):
         if not os.path.exists(output_dir):
             os.makedirs(output_dir)
 
-        formatter = HtmlFormatter(cssfile="pygments.css", style="one-dark")
-        css_content = formatter.get_style_defs(".codehilite")
+        css_content = settings.PYGMENTS_FORMATTER.get_style_defs("." + settings.PYGMENTS_CSS_CLASS)
 
         with open(output_file, "w") as f:
             f.write(css_content)
---
+++ static/scripts/script.js
@@ -0,0 +1,31 @@
+const copy_button_label = "Copy";
+
+$(() => {
+	let elements = $("pre");
+
+	elements.each((idx, element) => {
+		if (navigator.clipboard) {
+			let button = $("<button></button>");
+			$(element).children("code").after(button);
+
+			button.text(copy_button_label);
+			button.addClass("copy-code-button btn");
+
+			button.on("click", async () => {
+				await copy_code(element);
+			});
+		}
+	});
+
+	async function copy_code(element) {
+		let code = $(element).children("code");
+		let text = $(code).text();
+		let lines = text.split("\n");
+		lines = lines.map((line, idx) => {
+			return line.slice(idx.toString().length);
+		});
+		text = lines.join("\n");
+
+		await navigator.clipboard.writeText(text);
+	}
+});
