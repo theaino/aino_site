fe904e2a8d6adb67bfdafcba7285ea7cab14d99d
Aino Spring <info@aino-spring.com>
2024-10-04 19:48:26 +0200

    Default 100% width img

--- md_ext/image.py
+++ md_ext/image.py
@@ -7,18 +7,22 @@ class ImgInlineProcessor(InlineProcessor):
     def handleMatch(self, m, data):
         alt_text = m.group(1)
         url = m.group(2)
-        width = m.group(3)
-        height = m.group(4)
+        has_size = len(m.groups()) > 2
+        width = m.group(3) if has_size else ""
+        height = m.group(4) if has_size else ""
 
         el = etree.Element("img")
         el.set("src", url)
         el.set("alt", alt_text)
 
         styles = []
-        if width != "":
+        if width:
             styles.append(f"width:{width}")
-        if height != "":
+        if height:
             styles.append(f"height:{height}")
+
+        if not (width or height):
+            styles.append(f"width:100%")
 
         el.set("style", ";".join(styles))
 
@@ -27,7 +31,7 @@ class ImgInlineProcessor(InlineProcessor):
 
 class ImgExtension(Extension):
     def extendMarkdown(self, md):
-        IMAGE_RE = r"!\[(.*)\]\((.+) +(.*)x(.*)\)"
+        IMAGE_RE = r"!\[(.*)\]\(([^\s]+)(?: +(.*)x(.*))?\)"
         md.inlinePatterns.register(ImgInlineProcessor(IMAGE_RE, md), "sized-image", 175)
 
 
